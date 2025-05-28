605c15eb43db1640b016f64466ffe3f5ab034f0c
Mayoneso <62784004+Mayoneso-0@users.noreply.github.com>
2025-05-28 15:24:28 -0500

    Add files via upload

---
+++ Proyecto/Funciones.py
@@ -0,0 +1,124 @@
+#Importaciones necesarias
+import math
+
+# Definimos las variables necesarias para las funciones
+InicioDomX = 0
+FinalDomX = 0
+InicioDomY = 0
+FinalDomY = 0
+
+InicioRango = 0
+FinalRango = 0
+
+Funcion = None
+
+
+# Definimos las funciones matematicas que vamos a usar
+# Cada funcion tiene su dominio y rango, que se definen en las funciones de seleccion
+def ackleyFunction(x, y):
+    resultado = -20*math.exp(-0.2*(0.5*(x**2+y**2)**0.5)) \
+        -math.exp(0.5*(math.cos(2*math.pi*x)+math.cos(2*math.pi*y))) \
+        +math.e+20
+    return round(resultado,5)
+def seleccionarAckleyFunction():
+    global InicioDomX
+    InicioDomX = -5
+    global FinalDomX
+    FinalDomX = 5
+    global InicioDomY
+    InicioDomY = -5
+    global FinalDomY
+    FinalDomY = 5 
+
+    global InicioRango
+    InicioRango = 0
+    global FinalRango
+    FinalRango = 15
+
+    global Funcion
+    Funcion = ackleyFunction
+
+def mcCormickFunction(x, y):
+    resultado = (math.sin(x+y)) + (x-y)**2 - 1.5*x + 2.5*y + 1
+    return round(resultado,5)
+def seleccionarMcCormickFunction():
+    global InicioDomX
+    InicioDomX = -1.5
+    global FinalDomX
+    FinalDomX = 4
+    global InicioDomY
+    InicioDomY = -3
+    global FinalDomY
+    FinalDomY = 4
+
+    global InicioRango
+    InicioRango = -2
+    global FinalRango
+    FinalRango = 45
+
+    global Funcion
+    Funcion = mcCormickFunction
+
+def bukinN6Function(x, y):
+    resultado = 100*(abs(y-0.01*x**2))**0.5+0.01*abs(x+10)
+    return resultado
+def seleccionarBukinN6Function():
+    global InicioDomX
+    InicioDomX = -15
+    global FinalDomX
+    FinalDomX = -5
+    global InicioDomY
+    InicioDomY = -4
+    global FinalDomY
+    FinalDomY = 6
+
+    global InicioRango
+    InicioRango = 0
+    global FinalRango
+    FinalRango = 260
+
+    global Funcion
+    Funcion = bukinN6Function
+
+def leviN13Function(x, y):
+    resultado = (math.sin(3*math.pi*x))**2 + (x-1)**2 * (1 + (math.sin(3*math.pi*y)**2)) + (y-1)**2 * (1+(math.sin(2*math.pi*y)**2))
+    return round(resultado,5)
+def seleccionarLeviN13Function():
+    global InicioDomX
+    InicioDomX = -10
+    global FinalDomX
+    FinalDomX = 10
+    global InicioDomY
+    InicioDomY = -10
+    global FinalDomY
+    FinalDomY = 10
+
+    global InicioRango
+    InicioRango = 0
+    global FinalRango
+    FinalRango = 460
+
+    global Funcion
+    Funcion = leviN13Function
+
+def easomFunction(x, y):
+    resultado = -math.cos(x)*math.cos(y)*math.exp(-((x-math.pi)**2+(y-math.pi)**2))
+    return round(resultado,5)
+def seleccionarEasomFunction():
+    global InicioDomX
+    InicioDomX = -1
+    global FinalDomX
+    FinalDomX = 7
+    global InicioDomY
+    InicioDomY = -1
+    global FinalDomY
+    FinalDomY = 7
+
+    global InicioRango
+    InicioRango = -1
+    global FinalRango
+    FinalRango = 0.1
+
+    global Funcion
+    Funcion = easomFunction
+
