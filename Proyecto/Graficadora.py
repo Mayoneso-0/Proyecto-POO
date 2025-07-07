744a4352652b45ee79b2f93c549eafd5b0a7acfb
Mayoneso <62784004+Mayoneso-0@users.noreply.github.com>
2025-07-06 20:56:06 -0500

    Update Graficadora.py

--- Proyecto/Graficadora.py
+++ Proyecto/Graficadora.py
@@ -13,10 +13,17 @@ definicion_canva = 5
 definicion_colores = 100
 dibujo_particulas = []
 
-num_particulas = 8
+num_particulas = 12
 w = 0.4
-pp = 0.5
-pg = 0.7
+pp_inicial = 2.5
+pp_final = 0.5
+pg_inicial = 0.5
+pg_final = 2.5
+pp = pp_inicial
+pg = pg_inicial
+
+manual = False 
+seguir_iterando = True
 
 # Seleccionamos la funcion que queremos graficar
 f.seleccionar_rastrigin_function()
@@ -58,15 +65,54 @@ def iniciar_enjambre(event):
 window.bind("<q>", iniciar_enjambre)
 
 def iterar_algoritmo(event):
-    nuevo_pos_x, nuevo_pos_y = enjambre1.iterar_algorimo(ancho_canva=ancho_canva,
-                                                        alto_canva=alto_canva,
-                                                        w=w, pp=pp, pg=pg)
-    for i in range(0,len(enjambre1.particulas)):
-        canvas.move(dibujo_particulas[i],nuevo_pos_x[i],nuevo_pos_y[i])
+    global seguir_iterando,finalizar_algoritmo, pp, pg
+    num_coords_iguales = 0
+    mejor_coords_penultima = 0
+    mejor_cooords_ultima = (enjambre1.mejor_pos_global_x, enjambre1.mejor_pos_global_y)
+    # Si manual es True, iteramos solo una vez
+    if manual:
+        nuevo_pos_x, nuevo_pos_y = enjambre1.iterar_algorimo(ancho_canva=ancho_canva,
+                                                            alto_canva=alto_canva,
+                                                            w=w, pp=pp, pg=pg)
+        for i in range(0,len(enjambre1.particulas)):
+            canvas.move(dibujo_particulas[i],nuevo_pos_x[i],nuevo_pos_y[i])
+        # Actualizamos los valores de pp y pg por cada iteración
+        if pp > pp_final:
+                pp -= 0.1
+        if pg < pg_final:
+                pg += 0.1
+    # Si manual es False, iteramos cada 100ms hasta que converja
+    else:
+        while seguir_iterando:
+            nuevo_pos_x, nuevo_pos_y = enjambre1.iterar_algorimo(ancho_canva=ancho_canva,
+                                                                alto_canva=alto_canva,
+                                                                w=w, pp=pp, pg=pg)
+            for i in range(0,len(enjambre1.particulas)):
+                canvas.move(dibujo_particulas[i],nuevo_pos_x[i],nuevo_pos_y[i])
+            window.update()
+            window.after(100)
+            mejor_coords_penultima = mejor_cooords_ultima
+            # Comprobamos las coordenadas no cambien desde las ultimas 10 iteraciones
+            mejor_cooords_ultima = (enjambre1.mejor_pos_global_x, enjambre1.mejor_pos_global_y)
+            if mejor_coords_penultima == mejor_cooords_ultima:
+                num_coords_iguales += 1
+            else:
+                num_coords_iguales = 0
+            if num_coords_iguales >= 10:
+                seguir_iterando = False
+                print("El algoritmo ha convergido.")
+                finalizar_algoritmo(event)
+            # Actualizamos los valores de pp y pg por cada iteración
+            if pp > pp_final:
+                pp -= 0.1
+            if pg < pg_final:
+                pg += 0.1
 # Iteramos el algoritmo al presionar la tecla "w"
 window.bind("<w>", iterar_algoritmo)
 
 def finalizar_algoritmo(event):
+    global seguir_iterando
+    seguir_iterando = False
     window.destroy()
     print()
     print("Algoritmo finalizado.")
@@ -86,3 +132,4 @@ window.bind("<e>", finalizar_algoritmo)
 
 # Bucle de ventana
 window.mainloop()
+
